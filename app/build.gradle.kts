9b7e9397b27911c530053290369c07a047d3c64f
Tikhon <tihonbikbaev@gmail.com>
2024-03-31 23:45:39 +0300

    7.10(need to add dependency)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,7 +29,10 @@ android {
 }
 
 dependencies {
+    def room_version = "2.6.1"
 
+    implementation "androidx.room:room-runtime:$room_version"
+    annotationProcessor "androidx.room:room-compiler:$room_version"
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
@@ -37,4 +40,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
 }
