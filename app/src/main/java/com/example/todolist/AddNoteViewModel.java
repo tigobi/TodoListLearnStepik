f34a30b7c49d3037964aa419061c708712a77a21
Tikhon <tihonbikbaev@gmail.com>
2024-04-07 22:47:26 +0300

    8.1 its working!!!!!!!!

--- app/src/main/java/com/example/todolist/AddNoteViewModel.java
+++ app/src/main/java/com/example/todolist/AddNoteViewModel.java
@@ -7,6 +7,10 @@ import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.functions.Action;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
 public class AddNoteViewModel extends AndroidViewModel {
     private NotesDao notesDao;
     private MutableLiveData<Boolean> shouldCloseScreen = new MutableLiveData<>();
@@ -21,13 +25,14 @@ public class AddNoteViewModel extends AndroidViewModel {
     }
 
     public void saveNote(Note note) {
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                notesDao.add(note);
-                shouldCloseScreen.postValue(true);
-            }
-        });
-        thread.start();
+        notesDao.add(note)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action() {
+                    @Override
+                    public void run() throws Throwable {
+                        shouldCloseScreen.postValue(true);
+                    }
+                });
     }
 }
--- app/src/main/java/com/example/todolist/NotesDao.java
+++ app/src/main/java/com/example/todolist/NotesDao.java
@@ -7,13 +7,15 @@ import androidx.room.Query;
 
 import java.util.List;
 
+import io.reactivex.rxjava3.core.Completable;
+
 @Dao
 public interface NotesDao {
     @Query("SELECT * FROM notes")
     LiveData<List<Note>> getNotes();
 
     @Insert
-    void add(Note note);
+    Completable add(Note note);
 
     @Query("DELETE FROM notes WHERE id = :id")
     void remove(int id);
